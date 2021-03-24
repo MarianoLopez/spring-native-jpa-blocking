5b19d3989dfb13104dc73d32c19c923e40651aa9
MarianoLopez <m_villa@hotmail.com>
2021-03-24 11:50:38 -0300

    first commit

---
+++ src/main/java/com/z/nativejpablocking/NativeJpaBlockingApplication.java
@@ -0,0 +1,13 @@
+package com.z.nativejpablocking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativeJpaBlockingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NativeJpaBlockingApplication.class, args);
+	}
+
+}
